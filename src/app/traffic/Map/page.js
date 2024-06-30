24980f1fe8beefe84e2c3601287c91743d4bf687
Lin-Rexter <aaaa35963596@gmail.com>
2024-07-01 02:16:26 +0800

    update Map/page.js

--- src/app/traffic/Map/page.js
+++ src/app/traffic/Map/page.js
@@ -1,8 +1,14 @@
+'use client'
+import React, { useState, useEffect } from "react";
+import LocationAggregatorMap from '../../components/map';
+/*
+'use client'
 import dynamic from "next/dynamic";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "../store/page";
 
-const DynamicMap = dynamic(() => import("./Map"), {
+
+const DynamicMap = dynamic(() => import("../../components/map"), {
   ssr: false,
 });
 
@@ -15,3 +21,37 @@ export default function Home() {
     </div>
   );
 }
+  */
+
+const HomePage = () => {
+  const [details, setDetails] = useState([]);
+  const [coordinates, setCoordinates] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://api-shipx-pl.easypack24.net/v1/points?per_page=28000"
+      );
+
+      const data = await response.json();
+      setDetails(data.items);
+
+      // Create an array of geo coordinates pairs
+      const coords = data.items.map((item) => [
+        item.location.longitude,
+        item.location.latitude,
+      ]);
+      setCoordinates(coords);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className="relative min-h-screen">
+      <LocationAggregatorMap data={coordinates} />
+    </div>
+  )
+}
+
+export default HomePage
