05215d56052ba626daee8ac2571bb9699013beed
Lin-Rexter <aaaa35963596@gmail.com>
2024-07-12 04:09:34 +0800

    update GeoJSON Data Source

--- src/app/traffic/Map/page.js
+++ src/app/traffic/Map/page.js
@@ -1,54 +1,81 @@
 'use client'
 import React, { useState, useEffect } from "react";
-import LocationAggregatorMap from '../../components/map';
-/*
 import dynamic from "next/dynamic";
-import { Provider as ReduxProvider } from "react-redux";
-import store from "../store/page";
+//import LocationAggregatorMap from '../../components/map';
 
+/*
+  import { Provider as ReduxProvider } from "react-redux";
+  import store from "../store/page";
 
-const DynamicMap = dynamic(() => import("../../components/map"), {
-  ssr: false,
+  
+
+  export default function Home() {
+    return (
+      <div>
+        <ReduxProvider store={store}>
+          <DynamicMap />
+        </ReduxProvider>
+      </div>
+    );
+  }
+*/
+
+const Dynamic_LocationAggregatorMap = dynamic(() => import("../../components/map"), {
+  ssr: true,
 });
-
-export default function Home() {
-  return (
-    <div>
-      <ReduxProvider store={store}>
-        <DynamicMap />
-      </ReduxProvider>
-    </div>
-  );
-}
-  */
 
 const HomePage = () => {
   const [details, setDetails] = useState([]);
-  const [coordinates, setCoordinates] = useState([]);
+  //const [coordinates, setCoordinates] = useState([]);
 
+  /*
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(
-        "https://api-shipx-pl.easypack24.net/v1/points?per_page=28000"
+        "http://localhost:3000/api/tdx"
       );
 
       const data = await response.json();
-      setDetails(data.items);
+      //setDetails(data.items);
 
       // Create an array of geo coordinates pairs
+      
       const coords = data.items.map((item) => [
         item.location.longitude,
         item.location.latitude,
       ]);
       setCoordinates(coords);
+  
     };
 
-    getData();
+    //getData();
+  }, []);
+  */
+
+  const geojson_url = "http://localhost:3000/api/tdx"
+
+  useEffect(() => {
+    const geojson_result = async () => {
+      var response = await fetch(geojson_url, {
+        method: "GET",
+      }).then((res) => {
+        const data = res.json();
+        return data
+      }).then((data) => {
+        //console.log(data)
+        setDetails(data.section_geojson);
+        return data.section_geojson
+      }).catch((err) => {
+        console.log('錯誤:', err);
+      })
+    }
+
+    geojson_result()
   }, []);
 
   return (
     <div className="relative min-h-screen">
-      <LocationAggregatorMap data={coordinates} />
+      <Dynamic_LocationAggregatorMap data={details} />
     </div>
   )
 }
