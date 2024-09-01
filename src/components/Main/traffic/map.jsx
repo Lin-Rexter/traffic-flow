be6bdc5728cd5b26fc6a6b11334590fe09632f30
Lin-Rexter <aaaa35963596@gmail.com>
2024-09-02 03:04:51 +0800

    更新交通壅塞地圖組件 - Dynamic_LocationAggregatorMap組件

--- src/components/Main/traffic/map.jsx
+++ src/components/Main/traffic/map.jsx
@@ -4,6 +4,7 @@ import { Source, Layer, Map, Marker } from "react-map-gl"; // 替代方案: 'rea
 import "mapbox-gl/dist/mapbox-gl.css";
 //import 'maplibre-gl/dist/maplibre-gl.css';
 import DeckGL from 'deck.gl';
+import { Button, Toast } from "flowbite-react";
 import {
     lightingEffect,
     material,
@@ -24,6 +25,7 @@ const LocationAggregatorMap = ({ data, error, warn }) => {
     if (data) {
         last_data = data
     }
+    //console.log(last_data)
 
     // 建立提示框(tooltip)
     function getTooltip({ object }) {
@@ -45,12 +47,12 @@ const LocationAggregatorMap = ({ data, error, warn }) => {
         <div className="grid h-full w-full">
             {/* 地圖視覺化 */}
             <DeckGL
-                layers={Layer_GeoJson(last_data ? last_data.section_geojson : null)} // 自訂的視覺化圖層(GeoJson)
+                layers={Layer_GeoJson(last_data ? last_data : null)} // 自訂的視覺化圖層(GeoJson)
                 effects={[lightingEffect]}
                 initialViewState={INITIAL_VIEW_STATE}
                 controller={true}
                 getTooltip={getTooltip}
-                style={{width: '100%', height: '100%'}}
+                style={{ width: '100%', height: '100%' }}
             >
                 {/* 以MapBox地圖為基底 */}
                 <Map
@@ -64,26 +66,28 @@ const LocationAggregatorMap = ({ data, error, warn }) => {
                 </Map>
             </DeckGL>
             {error && (
-                <div className="flex m-auto">
-                    <div id="toast-default" className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-                        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
-                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z" />
-                            </svg>
-                            <span className="sr-only">錯誤訊息(狀態碼: {JSON.stringify(error.status)})</span>
+                <Toast>
+                    <div className="fixed top-24 right-4 z-[9999] max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800">
+                        <div className="flex items-start">
+                            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-900 dark:text-cyan-300">
+                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z" />
+                                </svg>
+                                <span className="sr-only">錯誤訊息</span>
+                            </div>
+                            <div className="ml-3 text-sm font-normal">
+                                <div className="mb-1 text-base font-semibold text-gray-900 dark:text-white">
+                                    <p>錯誤訊息 (狀態碼: {JSON.stringify(error.status)})</p>
+                                </div>
+                                <div className="mb-1 text-sm font-normal">
+                                    <p>{JSON.stringify(error.info)}</p>
+                                    <p>{JSON.stringify(error.error_data)}</p>
+                                </div>
+                            </div>
+                            <Toast.Toggle />
                         </div>
-                        <div className="ms-3 text-sm font-normal">
-                            <p>{JSON.stringify(error.data)}</p>
-                            <p>{JSON.stringify(error.error_data)}</p>
-                        </div>
-                        <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
-                            <span className="sr-only">關閉</span>
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                        </button>
                     </div>
-                </div>
+                </Toast>
             )}
             {warn && (
                 <div className="flex m-auto">
