a9dac8c5c185b844b61825680454dec47eb08b4d
Lin-Rexter <aaaa35963596@gmail.com>
2024-09-11 02:35:38 +0800

    更新實用函數

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -45,6 +45,6 @@ export const has = (object, key) => object ? hasOwnProperty.call(object, key) : 
 // 相差天數
 export const DiffDays = (date1, date2) => {
     const diffTime = date2 - date1;
-    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
     return diffDays;
 }
